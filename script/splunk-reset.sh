6b733962fa2c893af5f38e1928623e14f72eaa14
Mike Hixson <mike.hixson@endicia.com>
2021-02-25 13:25:34 -0800

    Timezone fixes

--- script/splunk-reset.sh
+++ script/splunk-reset.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# The docker image doesnt have tzdata package, therefore no concept of timezones
+# https://www.reddit.com/r/Splunk/comments/loktmw/time_wrong_even_with_the_correct_timezone_set_up/
+sudo microdnf -y --nodocs update tzdata
+
 # Set permissions on config locations so we can write
 sudo chmod -R go+rwx $SPLUNK_HOME/etc/system/
 sudo chmod -R go+rwx $SPLUNK_HOME/etc/apps/search/
 
 # Copy config files
-cp -r ../config/. $SPLUNK_HOME/etc/system/local/
+cp -r /splunkconfig/config/. $SPLUNK_HOME/etc/system/local/
 
 # Copy config files to search application
-cp -r ../search_config/. $SPLUNK_HOME/etc/apps/search/local/
+cp -r /splunkconfig/search_config/. $SPLUNK_HOME/etc/apps/search/local/
 
 # Restart
 sudo $SPLUNK_HOME/bin/splunk stop
